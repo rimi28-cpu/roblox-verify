12884eb57ddfae61e385b279df4176e7baf72827
rimi28-cpu <rimiikarashi@gmail.com>
2026-01-02 19:18:35 +0530

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  // Allow CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -8,10 +7,9 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
   
-  // Simple test response
   return res.status(200).json({
     status: 'ok',
-    message: 'API is working',
+    message: 'API is working!',
     timestamp: new Date().toISOString()
   });
 }
